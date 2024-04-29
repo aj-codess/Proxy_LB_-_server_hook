e81cf7705bc7971ddd032721dd99878cf9f2fecc
Joseph Kwame Agyei <jagyei339@gmail.com>
2024-04-29 16:42:13 +0000

    refactor

--- main.cpp
+++ main.cpp
@@ -5,6 +5,24 @@
 
 using namespace std;
 
+
+std::vector<char> virtual_buffer(20*1024);
+
+void data_2_grab(boost::asio::ip::tcp::socket& socket){
+
+    socket.async_read_some(boost::asio::buffer(virtual_buffer.data(),virtual_buffer.size()),[&](boost::system::error_code error_pool,std::size_t length){
+        if(!error_pool){
+            for(int i=0;i<length;i++){
+                cout<<virtual_buffer[i]<<endl;
+            };
+            data_2_grab(socket);
+        };
+    });
+
+};
+
+
+
 int main(){
     
 boost::system::error_code error_pool;
@@ -23,24 +41,12 @@ if(!error_pool){
 
 if(socket.is_open()){
 
+    data_2_grab(socket);
+
     std::string request="GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n";
     socket.write_some(boost::asio::buffer(request.data(),request.size()),error_pool);
 
-    socket.wait(socket.wait_read);
-
-    size_t bytes=socket.available();
-
-    if(bytes>0){
-        std::vector<char> vBuffer(bytes);
-        socket.read_some(boost::asio::buffer(vBuffer.data(),vBuffer.size()),error_pool);
-
-        for(auto c:vBuffer){
-            cout<<c;
-        };
-    }
-
 };
-
 
     return 0;
 }
